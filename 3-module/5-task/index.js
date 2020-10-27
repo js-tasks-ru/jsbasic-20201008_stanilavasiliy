e04581c10ea3c32080d632668ef2a149fc175dba
Basilio <stanilavasiliy@mail.ru>
2020-10-27 19:47:42 +0300

    module 3 task 5 is completed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,13 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.replace(/,/g, ' ').split(' ');
+  let arrInteger = arr.filter( item => isFinite(+item) && item != '' );
+  
+  arrInteger.sort( (a, b) => a - b );
+
+  return {
+    min: +arrInteger[0],
+    max: +arrInteger[arrInteger.length - 1],
+  }
 }
