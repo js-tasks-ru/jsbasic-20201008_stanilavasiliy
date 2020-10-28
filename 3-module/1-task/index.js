2e5e0c9aed0d045cbf4ae4472a2ca7854f3e0742
Basilio <stanilavasiliy@mail.ru>
2020-10-28 11:29:32 +0300

    module 3 task 1 is fixed

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,11 +5,3 @@
 function namify(users) {
   return users.map( user => user.name );
 }
-
-let vasya = { name: 'Вася', age: 25 };
-let petya = { name: 'Петя', age: 30 };
-let masha = { name: 'Маша', age: 28 };
-
-let users = [ vasya, petya, masha ];
-
-let names = namify(users);
