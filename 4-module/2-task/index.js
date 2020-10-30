b2b70419b8d24abf4f548955729221f2c94d5257
Basilio <stanilavasiliy@mail.ru>
2020-10-30 20:11:41 +0300

    module 4 task 2 is completed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,9 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    let tbody = table.children[0];
+
+    for (let i = 0; i < tbody.children.length; i++) {
+        tbody.rows[i].cells[i].style.background = 'red';
+    }
 }
