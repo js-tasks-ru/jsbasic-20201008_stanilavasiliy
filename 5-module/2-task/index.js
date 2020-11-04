a05fd12fdb2a729938b4f15e05ad2f2e0c01a831
Basilio <stanilavasiliy@mail.ru>
2020-11-04 10:57:59 +0300

    module 5 task 2 is completed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.getElementsByClassName('toggle-text-button')[0];
+  let div = document.getElementById('text');
+
+  toggleButton.addEventListener('click', () => {
+    div.hidden = !div.hidden;
+  });
 }
