ca1ebc5e0eb5dec13656dd3eed99bdafb11261a3
Basilio <stanilavasiliy@mail.ru>
2020-11-26 12:38:26 +0300

    module 8 task 1 is completed

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -5,6 +5,8 @@ export default class CartIcon {
     this.render();
 
     this.addEventListeners();
+
+    this.cartIconPosition = true;
   }
 
   render() {
@@ -39,6 +41,44 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (this.elem.offsetWidth) {
+
+      if (document.documentElement.clientWidth <= 767) {
+        return;
+      }
+
+      if (this.cartIconPosition) {
+        this.elemTop = window.pageYOffset + this.elem.getBoundingClientRect().top;
+        this.elemLeft = this.elem.getBoundingClientRect().left;
+        this.cartIconPosition = false;
+      }
+
+      if (this.elemTop > window.pageYOffset) {
+
+        this.elem.style.position = 'fixed';
+        this.elem.style.left = this.elemLeft + 'px';
+        this.elem.style.top = this.elemTop + 'px';
+        console.log(this.elemLeft);
+
+      } else {
+
+        let firstContainerElementRight = document.querySelectorAll('.container')[0]
+                                        .firstElementChild.getBoundingClientRect().right;
+        let coordElem = document.documentElement.clientWidth - this.elem.getBoundingClientRect().width;
+        
+        this.elem.style.position = 'fixed';
+        this.elem.style.zIndex = 1000;
+
+        if (firstContainerElementRight + 20 > coordElem - 10) {
+          this.elem.style.left = coordElem - 10 + 'px';
+        } else {
+          this.elem.style.left = firstContainerElementRight + 20 + 'px';
+        }
+
+        if (this.elem.getBoundingClientRect().top < 50) {
+          this.elem.style.top = '50px';
+        }
+      }
+    }
   }
 }
